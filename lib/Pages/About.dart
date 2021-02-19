3c6827e2ec706c6daa9674efaa364732cacdb5b3
Jcupzz <jcupzz255@gmail.com>
2021-02-19 12:03:49 +0530

    About screen done!

--- lib/Pages/About.dart
+++ lib/Pages/About.dart
@@ -11,22 +11,30 @@ class About extends StatefulWidget {
 
 class _AboutState extends State<About> {
   bool isLoaded = false;
+
   void initState() {
     super.initState();
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
+  int _indexStack = 1;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: (isLoaded)?WebView(
-        onPageFinished: (String url){
-          setState(() {
-            isLoaded = true;
-          });
-        },
-        javascriptMode: JavascriptMode.unrestricted,
-        initialUrl: 'https://www.notion.so/Lefty-fc3ef66584314d1a8f4a50bd16574a5d',
-      ):Circular_Loading(),
+    return IndexedStack(
+      index: _indexStack,
+      children: [
+        SafeArea(
+          child: WebView(
+            onPageFinished: (String url) {
+              setState(() {
+                _indexStack = 0;
+              });
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: 'https://www.notion.so/Lefty-fc3ef66584314d1a8f4a50bd16574a5d',
+          ),
+        ),
+        Circular_Loading(),
+      ],
     );
   }
 }
